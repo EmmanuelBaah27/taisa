a26113b696aa5fcfc836e01bc67992427b3f19f2
emmanuelbaah27 <emmanuelbaah27@gmail.com>
2026-08-20 18:07:21 +0000

    fix: preserve reply long press surface

--- mobile/src/components/ui/TaisaReplyCard.tsx
+++ mobile/src/components/ui/TaisaReplyCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { colors } from '../../constants/theme';
 import type { ResponseReaction } from '../../repositories/responseFeedbackRepository';
 import { LiquidGlassPressable } from './LiquidGlassPressable';
